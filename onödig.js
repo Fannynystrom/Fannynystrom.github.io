920a2cd2410c52699ab7c2bdfc8d75f8cd6b7cbe
Fannosa <fannynystrom@outlook.comls>
2023-10-02 15:15:35 +0200

    deleted images

---
+++ onödig.js
@@ -0,0 +1,8 @@
+let text = "Fanny" 
+
+console.log(text)
+for (let i = 0; i < 10; i++) {
+    text += "The number is" + i + "<br>";
+    console.log("Fanny")
+
+}
---
+++ variables.js
@@ -0,0 +1,12 @@
+x = 2
+let secret =(3*x+4)
+console.log(secret)
+
+const message = "hello world"
+console.log(message)
+
+let = allowedToEnter = true 
+console.log(allowedToEnter)
+
+let =allowedToEnter = false
+console.log (allowedToEnter)
